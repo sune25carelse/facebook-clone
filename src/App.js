6f1128135a01a843bfe1e7241e1c4b64e6ee43d3
Sune Carelse <Sune.Carelse@hq.takealot.com>
2020-11-16 21:56:38 +0200

    created search bar and icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CleverProgrammers</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
+import  SubscriptionsOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined';
+import  StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined';
+import  SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import { Avatar, Iconbutton }from '@material-ui/core'
+import AddIcon from '@material-ui/icons/Add'
+import ForumIcon from '@material-ui/icons/Forum'
+import NotificationsActive from '@material-ui/icons/NotificationsActive'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+
+
+const Header = () => {
+  return (
+    <div className='header' >
+      <div className="header__left">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1200px-Facebook_Logo_%282019%29.png"
+        alt="Facebook logo" />
+      </div>
+      <div className="header__input">
+        <SearchIcon />
+        <input placeholder='Search Facebook' type="text" />
+      </div>
+      <div className="header__center">
+          <div className="header__option
+          header__option--active">
+              <HomeIcon fontsize='large' />
+          </div>
+          <div className="header__option">
+              <FlagIcon fontsize='large' />
+          </div>
+          <div className="header__option">
+              <SubscriptionsOutlinedIcon fontsize='large' />
+          </div>
+          <div className="header__option">
+              <StorefrontOutlinedIcon fontsize='large' />
+          </div>
+          <div className="header__option">
+              <SupervisedUserCircleIcon fontsize='large' />
+          </div>
+      </div>
+
+      <div className="header__right">
+          <div className="header__info">
+              <Avatar />
+              <h4>Fabba</h4>
+          </div>
+          <AddIcon />
+
+          <ForumIcon />
+
+          <NotificationsActive />
+
+          <ExpandMoreIcon />
+
+      </div>
+    </div>  
+  );
+}
+
+export default Header;
+
