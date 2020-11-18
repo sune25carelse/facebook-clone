201387db285e106b39c9d4299a7e5b832609d37a
Sune Carelse <Sune.Carelse@hq.takealot.com>
2020-11-18 21:47:21 +0200

    updated story reel with the images

---
+++ src/Components/StoryReel.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Story from './Story';
+
+const StoryReel = () => {
+  return (
+    <div className='storyReel'>
+        <Story
+            image='https://seeklogo.net/wp-content/uploads/2011/08/liverpool-logo-vector-400x400.png'
+            profileSrc='https://seeklogo.net/wp-content/uploads/2012/11/liverbir-vector-logo-400x400.png'
+            title='Fabba1892'
+        />
+    </div>
+  );
+}
+
+export default StoryReel;
