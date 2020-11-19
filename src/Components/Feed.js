cec1e5c547f7d0253fdff34b90b4bf76984f98a3
Sune Carelse <Sune.Carelse@hq.takealot.com>
2020-11-19 23:04:56 +0200

    add Post.js and info

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import StoryReel from './StoryReel';
 import MessageSender from './MessageSender';
+import Post from './Post';
 
 
 const Feed = () => {
@@ -8,6 +9,14 @@ const Feed = () => {
     <div className='feed'>
         <StoryReel />
         <MessageSender />
+        
+        <Post 
+          profilePic='https://seeklogo.net/wp-content/uploads/2012/11/liverbir-vector-logo-400x400.png'
+          message='yoo this a message'
+          timestamp='time'
+          imgName='imgName'
+          username='Fabba1892'
+        />
 
         {/*
             postsData.map(entry => (
